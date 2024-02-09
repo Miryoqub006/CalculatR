66ddf44418577e446db21db3543fbc25edc6599b
Miryoqub <salimovmiryoqub@gmail.com>
2024-02-09 22:03:50 +0500

    Beta

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿string userInput = Console.ReadLine();
+
+int plusIndex = userInput.IndexOf("+");
+int minusIndex = userInput.IndexOf("-");
+int kopIndex = userInput.IndexOf("*+");
+int bolIndex = userInput.IndexOf("/");
+int darajaIndex = userInput.IndexOf("^");
+
+int firstNum = Convert.ToInt32(userInput.Substring(0, 1));
+
+int secNum = Convert.ToInt32(userInput.Substring(2, 1));
+
+if (plusIndex >= 0)
+{
+    Console.WriteLine($" {firstNum + secNum}  ");
+}
+else if (minusIndex >= 0)
+{
+    Console.WriteLine($" {firstNum - secNum}  ");
+}
+else if (bolIndex >= 0)
+{
+    Console.WriteLine($" {firstNum / secNum}  ");
+}
+else if (kopIndex >= 0)
+{
+    Console.WriteLine($" {firstNum * secNum}  ");
+}
+else if (darajaIndex >= 0)
+{
+    Console.WriteLine($" {Math.Pow(firstNum, secNum)}  ");
+}
